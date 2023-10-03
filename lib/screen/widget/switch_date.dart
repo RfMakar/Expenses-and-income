bc3c5a1bf181ee83ff0aab182ddc18a3e42527fa
Maksim <rfmakar@gmail.com>
2023-10-03 23:30:27 +0300

    Изменения в widget_switch_date

--- lib/screen/widget/switch_date.dart
+++ lib/screen/widget/switch_date.dart
@@ -36,61 +36,73 @@ class _SwitchDateState extends State<SwitchDate> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-                splashRadius: 10,
-                icon: const Icon(Icons.navigate_before),
-                onPressed: () {
-                  //Если месяц и год не 01.2021 то дата переключится на месяц назад
-                  var enabledButton =
-                      (dateTime.year == 2021) && (dateTime.month == 1);
-                  if (!enabledButton) {
-                    dateTime = DateTime(
-                      dateTime.year,
-                      dateTime.month - 1,
-                    );
-                  }
-                  setState(() {
-                    widget.onPressedCallBack(dateTime);
-                  });
-                }),
-            Column(
-              children: [
-                Text(
-                  widget.value,
-                  style: const TextStyle(fontSize: 24),
-                ),
-                Text(
-                  getDate(),
-                  style: const TextStyle(fontSize: 10),
-                ),
-              ],
-            ),
-            IconButton(
-              splashRadius: 10,
-              onPressed: () {
-                //Если дата не текущая то прибавить месяц
-                var enabledButton = (dateTime.year == currentDate.year) &&
-                    (dateTime.month == currentDate.month);
-                if (!enabledButton) {
-                  dateTime = DateTime(
-                    dateTime.year,
-                    dateTime.month + 1,
-                  );
-                }
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: [
+              const SizedBox(height: 10),
+              Text(
+                widget.value,
+                style:
+                    const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    color: Colors.grey,
+                    icon: const Icon(Icons.navigate_before),
+                    onPressed: () {
+                      //Если месяц и год не 01.2021 то дата переключится на месяц назад
+                      var enabledButton =
+                          (dateTime.year == 2021) && (dateTime.month == 1);
+                      if (!enabledButton) {
+                        dateTime = DateTime(
+                          dateTime.year,
+                          dateTime.month - 1,
+                        );
+                      }
+                      setState(() {
+                        widget.onPressedCallBack(dateTime);
+                      });
+                    },
+                  ),
+                  Text(
+                    getDate(),
+                    style: const TextStyle(color: Colors.grey),
+                  ),
+                  IconButton(
+                    color: Colors.grey,
+                    onPressed: () {
+                      //Если дата не текущая то прибавить месяц
+                      var enabledButton = (dateTime.year == currentDate.year) &&
+                          (dateTime.month == currentDate.month);
+                      if (!enabledButton) {
+                        dateTime = DateTime(
+                          dateTime.year,
+                          dateTime.month + 1,
+                        );
+                      }
 
-                setState(() {
-                  widget.onPressedCallBack(dateTime);
-                });
-              },
-              icon: const Icon(Icons.navigate_next),
+                      setState(() {
+                        widget.onPressedCallBack(dateTime);
+                      });
+                    },
+                    icon: const Icon(Icons.navigate_next),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Center(
+            child: IconButton(
+              color: Colors.grey,
+              onPressed: () {},
+              icon: const Icon(Icons.date_range_outlined),
+              iconSize: 30,
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
